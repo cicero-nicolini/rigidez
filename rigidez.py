ee5d37913bf56c6bcedbc36dc61fcc4995a290ba
Cícero Nicolini <ciceroaugusto12@gmail.com>
2026-05-01 21:38:54 -0300

    Estrutura para pontos e barras

--- rigidez.py
+++ rigidez.py
@@ -2,20 +2,46 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.collections import LineCollection
 
-lines = [
-	[(0, 1), (1, 1)],
-	[(2, 3), (3, 3)],
-	[(1, 2), (1, 3)],
+meu_nos = [
+    (20,40),
+    (40,40),
+    (20,20),
+    (40,20),
 ]
 
-xpoints = np.array([1, 8])
-ypoints = np.array([3, 10])
+meu_barras = [
+    (0,1),
+    (1,3),
+    (2,3),
+    (0,2),
+]
+
+def desenha_estrutura(nos, barras):
+    cord_x = []
+    cord_y = []
+
+    for no in nos:
+        x, y = no
+        cord_x.append(x)
+        cord_y.append(y)
+
+    linhas = []
+
+    for conec in barras:
+        indice_inicio, indice_fim = conec
+        coord_inicio = nos[indice_inicio]
+        coord_fim = nos[indice_fim]
+        linhas.append([coord_inicio, coord_fim])
+
+    lc = LineCollection(linhas, linewidths=2)
+    fig, ax = plt.subplots()
+    ax.add_collection(lc)
+    ax.plot(cord_x, cord_y, 'ro')
+    plt.show()
 
 
-lc = LineCollection(lines, linewidths=2)
-fig, ax = plt.subplots()
-ax.add_collection(lc)
-ax.plot(xpoints, ypoints, 'ro')
-ax.autoscale()
-ax.margins(0.1)
-plt.show()
+
+desenha_estrutura(meu_nos, meu_barras)
+
+
+
