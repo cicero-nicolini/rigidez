f5bd4e27ab2f81bf200613190a52acf1352c0739
Cícero Nicolini <ciceroaugusto12@gmail.com>
2026-05-20 00:46:49 -0300

    Criando classe nó e barra

--- rigidez.py
+++ rigidez.py
@@ -1,37 +1,49 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 from matplotlib.collections import LineCollection
 
+
+class No:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Barra: 
+    def __init__(self, no1, no2):
+        self.indice_no1 = no1
+        self.indice_no2 = no2        
+
 meu_nos = [
-    (20,40),
-    (40,40),
-    (20,20),
-    (40,20),
+    No(10,40),
+    No(30,40),
+    No(15,20),
+    No(20,20),
 ]
 
 meu_barras = [
-    (0,1),
-    (1,3),
-    (2,3),
-    (0,2),
+    Barra(0,1),
+    Barra(1,3),
+    Barra(2,3),
+    Barra(0,2),
 ]
 
 def desenha_estrutura(nos, barras):
-    cord_x = []
-    cord_y = []
+    cord_x = [] 
+    cord_y = [] 
 
     for no in nos:
-        x, y = no
-        cord_x.append(x)
-        cord_y.append(y)
+        cord_x.append(no.x) 
+        cord_y.append(no.y) 
 
     linhas = []
 
-    for conec in barras:
-        indice_inicio, indice_fim = conec
-        coord_inicio = nos[indice_inicio]
-        coord_fim = nos[indice_fim]
-        linhas.append([coord_inicio, coord_fim])
+    for barra in barras:
+        no_inicio = nos[barra.indice_no1] #coordenadas primeiro nó da barra
+        no_fim = nos[barra.indice_no2] #coordenadas segundo nó da barra
+        no1 = (no_inicio.x, no_inicio.y)
+        no2 = (no_fim.x, no_fim.y)
+        linhas.append([no1, no2])
 
     lc = LineCollection(linhas, linewidths=2)
     fig, ax = plt.subplots()
@@ -42,6 +54,31 @@ def desenha_estrutura(nos, barras):
 
 
 desenha_estrutura(meu_nos, meu_barras)
+
+def comprimento_barra(no1, no2):
+    dx = no2.x - no1.x
+    dy = no2.y - no1.y
+
+    return math.sqrt( dx * dx + dy * dy )
+
+
+for barra in meu_barras:
+    no_inicio = meu_nos[barra.indice_no1] #coordenadas primeiro nó da barra
+    no_fim = meu_nos[barra.indice_no2] #coordenadas segundo nó da barra 
+    tamanho = comprimento_barra(no_inicio, no_fim)
+    print(tamanho)
+
+
+
+
+    
+
+
+
+
+
+    
+
 
 
 
