3befd4d25292eea93edec56e578909feedae0391
Cícero Nicolini <ciceroaugusto12@gmail.com>
2026-05-25 17:18:38 -0300

    codigo adaptado quevedo

--- rigidez.py
+++ rigidez.py
@@ -5,167 +5,269 @@ from matplotlib.collections import LineCollection
 
 
 class No:
-    def __init__(self, x, y):
+    def __init__(self, num, x, y):
+        self.num = num 
         self.x = x
         self.y = y
 
 class Barra: 
-    def __init__(self, no1, no2):
-        self.indice_no1 = no1
-        self.indice_no2 = no2   
+    def __init__(self, num, no1, no2,E,A,I):
+        self.num = num
+        self.noi = no1
+        self.noj = no2
+        self.E = E
+        self.A = A
+        self.I = I
+        self.L = None      
+        self.kl = np.zeros((6,6))
+        self.r = np.zeros((6,6))
+        self.fepl = np.zeros((6))
+
+    def comprimento_barra(self): #argamentos são coordenadas dos nós
+        dx = self.noj.x - self.noi.x
+        dy = self.noj.y - self.noi.y
+        self.L = math.sqrt( dx * dx + dy * dy )
+
+    def calcula_klocal(self):
+        
+        a1 = self.E*self.A/self.L
+        a2 = (12.0*self.E*self.I)/self.L**3
+        a3 = (6.0*self.E*self.I)/self.L**2
+        a4 = (4.0*self.E*self.I)/self.L
+        a5 = (2.0*self.E*self.I)/self.L
+
+        self.kl[0,0] = a1
+        self.kl[0,4] = -a1
+        self.kl[1,1] = a2
+        self.kl[1,2] = a3
+        self.kl[1,4] = -a2
+        self.kl[1,5] = a3
+        self.kl[2,2] = a4
+        self.kl[2,4] = -a3
+        self.kl[2,5] = a5
+        self.kl[3,3] = a1
+        self.kl[4,4] = a2
+        self.kl[4,5] = -a3
+        self.kl[5,5] = a4
+
+        self.kl[4,0] = self.kl[0,4]
+        self.kl[2,1] = self.kl[1,2]
+        self.kl[4,1] = self.kl[1,4]
+        self.kl[5,1] = self.kl[1,5]
+        self.kl[4,2] = self.kl[2,4]
+        self.kl[5,2] = self.kl[2,5]
+        self.kl[5,4] = self.kl[4,5]        
+
+    def calcula_r(self): 
+        dx = self.noj.x - self.noi.x
+        dy = self.noj.y - self.noi.y
+        s = dy/self.L
+        c = dx/self.L
+
+        self.r[0,0] = c
+        self.r[1,0] = -s
+        self.r[0,1] = s
+        self.r[1,1] = c   
+        self.r[2,2] = 1
+        self.r[3,3] = c
+        self.r[4,3] = -s
+        self.r[3,4] = s      
+        self.r[4,4] = c
+        self.r[5,5] = 1 
+    
+    def calcula_fepl(self):
+        lw = self.L
+
+class Carregamento_distribuido: 
+     
+    def __init__(self, a, lw, w1, w2, barra):
+        self.a = a
+        self.lw = lw
+        self.w1 = w1
+        self.w2 = w2
+        self.barra = barra
+        
+    def calcula_Fepl(self):
+
+
+
+
+        self.barra.fepl[0] = self.barra.fepl[0]+ 1 
+
+        
+
+no1 = No(1,60,135)
+no2 = No(2,160,135)
+
+barra1 = Barra(1,no1,no2,10000,2*5,1000)
+barra1.comprimento_barra()
+barra1.calcula_klocal()
+barra1.calcula_r()
+print(barra1.kl)
+print(barra1.r)
+carregamento1 = Carregamento_distribuido(10,20,30,40,barra1)
+print(barra1.fepl)
+carregamento1.mostre()
+carregamento1.mostre()
+carregamento1.mostre()
+print(barra1.fepl)
+
+#    E = 10000.0
+#    area = 2.0 * 5.0
+#    inercia = 1000.0
+
 
 #class Estrutura:
- 
 
 
-meu_nos = [
-    No(60,135),
-    No(160,135),
-    No(260,60),
-    #No(20,20),
-]
-
-meu_barras = [
-    Barra(0,1),
-    Barra(1,2),
-    #Barra(2,3),
-    #Barra(0,2),
-]
-
-#cargas = []
-
-def comprimento_barra(no1, no2): #argamentos são coordenadas dos nós
-    dx = no2.x - no1.x
-    dy = no2.y - no1.y
-
-    return math.sqrt( dx * dx + dy * dy )
-
-def momento_inercia(b, l):
-
-    return b * l**3/12
-
-def matriz_local(E, A, I, L):
-
-    k = np.array([
-        [ (E*A)/L,            0.0,                 0.0,  -(E*A)/L,               0.0,                 0.0],
-
-        [     0.0,      (12.0*E*I)/L**3,  6.0*E*I/L**2,       0.0,  -(12.0*E*I)/L**3,      (6.0*E*I)/L**2],
-
-        [     0.0,       (6.0*E*I)/L**2,   (4.0*E*I)/L,       0.0,   -(6.0*E*I)/L**2,         (2.0*E*I)/L],
-
-        [-(E*A)/L,                  0.0,           0.0,   (E*A)/L,               0.0,                 0.0],
-
-        [     0.0,     -(12.0*E*I)/L**3,   -6*E*I/L**2,       0.0,   (12.0*E*I)/L**3,     -(6.0*E*I)/L**2],
-
-        [     0.0,       (6.0*E*I)/L**2,       2*E*I/L,       0.0,   -(6.0*E*I)/L**2,         (4.0*E*I)/L]
-    ])
-
-    return k
-
-def matriz_R(no1, no2): #argamentos são coordenadas dos nós
-
-    dx = no2.x - no1.x
-    dy = no2.y - no1.y
-    l = math.sqrt( dx * dx + dy * dy )
-    seno = dy/l
-    cos = dx/l
-
-    r = np.array([
-        [  cos,             seno,           0.0,             0.0,            0.0,           0.0],
-
-        [-seno,              cos,           0.0,             0.0,            0.0,           0.0],
-
-        [  0.0,              0.0,           1.0,             0.0,            0.0,           0.0],
-
-        [  0.0,              0.0,           0.0,             cos,           seno,           0.0],
-
-        [  0.0,              0.0,           0.0,           -seno,            cos,           0.0],
-
-        [  0.0,              0.0,           0.0,             0.0,            0.0,           1.0]
-    ])
-
-    return r
-
-def Fep_local(tipo, valor, no1, no2): 
-
-    """
-    argumentos:
-
-    tipo: distribuida ou pontual
-    coordenadas dos nós
-
-    """
-    dx = no2.x - no1.x
-    dy = no2.y - no1.y
-    l = math.sqrt( dx * dx + dy * dy )
-    seno = dy/l
-    cos = dx/l
-
-    if tipo == "distribuida":
-        qx = valor*seno
-        qy = valor*cos 
-        ha = (qx*l)/2.0
-        va = (qy*l)/2.0
-        ma = (qy*l*l)/12.0
-        hb = (qx*l)/2.0
-        vb = (qy*l)/2.0
-        mb = -(qy*l*l)/12.0    
-        Fep_local = np.array([ha, va, ma, hb, vb, mb])
-        return Fep_local
-
-    elif tipo == "pontual":
-        px = valor*seno
-        py = valor*cos
-        ha = -px/2.0
-        va =  py/2.0
-        ma =  (py*l)/8.0
-        hb = -px/2.0
-        vb =  py/2.0
-        mb = -(py*l)/8.0    
-        Fep_local = np.array([ha, va, ma, hb, vb, mb])
-        return Fep_local
-
-def desenha_estrutura(nos, barras):
-    cord_x = [] 
-    cord_y = [] 
-
-    for no in nos:
-        cord_x.append(no.x) 
-        cord_y.append(no.y) 
-
-    linhas = []
-
-    for barra in barras:
-        no_inicio = nos[barra.indice_no1] #coordenadas primeiro nó da barra
-        no_fim = nos[barra.indice_no2] #coordenadas segundo nó da barra
-        no1 = (no_inicio.x, no_inicio.y)
-        no2 = (no_fim.x, no_fim.y)
-        linhas.append([no1, no2])
-
-    lc = LineCollection(linhas, linewidths=2)
-    fig, ax = plt.subplots()
-    ax.add_collection(lc)
-    ax.plot(cord_x, cord_y, 'ro')
-    plt.show()
-
-desenha_estrutura(meu_nos, meu_barras)
-
-for barra in meu_barras:
-    no_inicio = meu_nos[barra.indice_no1] #coordenadas primeiro nó da barra
-    no_fim = meu_nos[barra.indice_no2] #coordenadas segundo nó da barra
-    R = matriz_R(no_inicio, no_fim)
-    R_T = np.linalg.inv(R)   
-    E = 10000.0
-    area = 2.0 * 5.0
-    inercia = 1000.0
-    tamanho = comprimento_barra(no_inicio, no_fim)
-    KL = matriz_local(E, area, inercia, tamanho)
-    K = R_T @ KL @ R
-    print(tamanho)
-
-tipo = "distribuida"
-fep = Fep_local(tipo, 0.24, no_inicio, no_fim)
-print(fep)
+#meu_nos = [
+#    No(60,135),
+#    No(160,135),
+#    No(260,60),
+#    #No(20,20),
+#]
+#
+#meu_barras = [
+#    Barra(0,1),
+#    Barra(1,2),
+#    #Barra(2,3),
+#    #Barra(0,2),
+#]
+#
+##cargas = []
+#
+#def comprimento_barra(no1, no2): #argamentos são coordenadas dos nós
+#    dx = no2.x - no1.x
+#    dy = no2.y - no1.y
+#
+#    return math.sqrt( dx * dx + dy * dy )
+#
+#def momento_inercia(b, l):
+#
+#    return b * l**3/12
+#
+#def matriz_local(E, A, I, L):
+#
+#    k = np.array([
+#        [ (E*A)/L,            0.0,                 0.0,  -(E*A)/L,               0.0,                 0.0],
+#
+#        [     0.0,      (12.0*E*I)/L**3,  6.0*E*I/L**2,       0.0,  -(12.0*E*I)/L**3,      (6.0*E*I)/L**2],
+#
+#        [     0.0,       (6.0*E*I)/L**2,   (4.0*E*I)/L,       0.0,   -(6.0*E*I)/L**2,         (2.0*E*I)/L],
+#
+#        [-(E*A)/L,                  0.0,           0.0,   (E*A)/L,               0.0,                 0.0],
+#
+#        [     0.0,     -(12.0*E*I)/L**3,   -6*E*I/L**2,       0.0,   (12.0*E*I)/L**3,     -(6.0*E*I)/L**2],
+#
+#        [     0.0,       (6.0*E*I)/L**2,       2*E*I/L,       0.0,   -(6.0*E*I)/L**2,         (4.0*E*I)/L]
+#    ])
+#
+#    return k
+#
+#def matriz_R(no1, no2): #argamentos são coordenadas dos nós
+#
+#    dx = no2.x - no1.x
+#    dy = no2.y - no1.y
+#    l = math.sqrt( dx * dx + dy * dy )
+#    seno = dy/l
+#    cos = dx/l
+#
+#    r = np.array([
+#        [  cos,             seno,           0.0,             0.0,            0.0,           0.0],
+#
+#        [-seno,              cos,           0.0,             0.0,            0.0,           0.0],
+#
+#        [  0.0,              0.0,           1.0,             0.0,            0.0,           0.0],
+#
+#        [  0.0,              0.0,           0.0,             cos,           seno,           0.0],
+#
+#        [  0.0,              0.0,           0.0,           -seno,            cos,           0.0],
+#
+#        [  0.0,              0.0,           0.0,             0.0,            0.0,           1.0]
+#    ])
+#
+#    return r
+#
+#def Fep_local(tipo, valor, no1, no2): 
+#
+#    """
+#    argumentos:
+#
+#    tipo: distribuida ou pontual
+#    coordenadas dos nós
+#
+#    """
+#    dx = no2.x - no1.x
+#    dy = no2.y - no1.y
+#    l = math.sqrt( dx * dx + dy * dy )
+#    seno = dy/l
+#    cos = dx/l
+#
+#    if tipo == "distribuida":
+#        qx = valor*seno
+#        qy = valor*cos 
+#        ha = (qx*l)/2.0
+#        va = (qy*l)/2.0
+#        ma = (qy*l*l)/12.0
+#        hb = (qx*l)/2.0
+#        vb = (qy*l)/2.0
+#        mb = -(qy*l*l)/12.0    
+#        Fep_local = np.array([ha, va, ma, hb, vb, mb])
+#        return Fep_local
+#
+#    elif tipo == "pontual":
+#        px = valor*seno
+#        py = valor*cos
+#        ha = -px/2.0
+#        va =  py/2.0
+#        ma =  (py*l)/8.0
+#        hb = -px/2.0
+#        vb =  py/2.0
+#        mb = -(py*l)/8.0    
+#        Fep_local = np.array([ha, va, ma, hb, vb, mb])
+#        return Fep_local
+#
+#def desenha_estrutura(nos, barras):
+#    cord_x = [] 
+#    cord_y = [] 
+#
+#    for no in nos:
+#        cord_x.append(no.x) 
+#        cord_y.append(no.y) 
+#
+#    linhas = []
+#
+#    for barra in barras:
+#        no_inicio = nos[barra.indice_no1] #coordenadas primeiro nó da barra
+#        no_fim = nos[barra.indice_no2] #coordenadas segundo nó da barra
+#        no1 = (no_inicio.x, no_inicio.y)
+#        no2 = (no_fim.x, no_fim.y)
+#        linhas.append([no1, no2])
+#
+#    lc = LineCollection(linhas, linewidths=2)
+#    fig, ax = plt.subplots()
+#    ax.add_collection(lc)
+#    ax.plot(cord_x, cord_y, 'ro')
+#    plt.show()
+#
+#desenha_estrutura(meu_nos, meu_barras)
+#
+#for barra in meu_barras:
+#    no_inicio = meu_nos[barra.indice_no1] #coordenadas primeiro nó da barra
+#    no_fim = meu_nos[barra.indice_no2] #coordenadas segundo nó da barra
+#    R = matriz_R(no_inicio, no_fim)
+#    R_T = np.linalg.inv(R)   
+#    E = 10000.0
+#    area = 2.0 * 5.0
+#    inercia = 1000.0
+#    tamanho = comprimento_barra(no_inicio, no_fim)
+#    KL = matriz_local(E, area, inercia, tamanho)
+#    K = R_T @ KL @ R
+#    print(tamanho)
+#
+#tipo = "distribuida"
+#fep = Fep_local(tipo, 0.24, no_inicio, no_fim)
+#print(fep)
 
 
 
