f5a19ff18bbf84ccff0e83e679b1c2d0f646c5cd
Cícero Nicolini <ciceroaugusto12@gmail.com>
2026-06-06 23:21:13 -0300

    adiciona condicao de contorno para vetor de forcas da estrutura

--- rigidez.py
+++ rigidez.py
@@ -23,6 +23,7 @@ class Barra:
         self.k = np.zeros((6,6))
         self.r = np.zeros((6,6))
         self.fepl = np.zeros((6))
+        self.fep = np.zeros((6))
         self.q = np.zeros((6),dtype=int)
 
     def comprimento_barra(self):
@@ -91,12 +92,13 @@ class Barra:
         
         
 class Estrutura:
-    def __init__(self, barras,nnos,grestritos):
+    def __init__(self,barras,nnos,grestritos):
         self.barras = barras
         self.nnos = nnos
         self.grestritos = grestritos
+        self.fnos = np.zeros((nnos*3))
+        self.f01 = np.zeros((nnos*3))
         self.k = np.zeros((nnos*3,nnos*3), dtype=int)
-        self.k_sobreposto = np.zeros((nnos*3,nnos*3), dtype=int)
         
     def calcula_k(self):
         for barra in self.barras:
@@ -110,28 +112,20 @@ class Estrutura:
                     self.k[barra.q[j], barra.q[jk]] = self.k[barra.q[j], barra.q[jk]]+barra.k[j,jk]
 
         print(self.k)
-        print(self.k_sobreposto)
 
     def calcula_k01(self):
         k01 = self.k
-        inicio = 0
-       #for i in range (0,3*self.nnos):
-       #    for j in range (inicio,3*self.nnos):
-       #        k01[i,j] = 0
-       #
+
         for g in self.grestritos:
-            for i in range (inicio,3*self.nnos):
-                for j in range (inicio,3*self.nnos):
+            for i in range (0,3*self.nnos):
+                for j in range (0,3*self.nnos):
                     if i == g or j == g:
                         k01[i,j] = 0
+                    if i == j and k01[i,j] == 0:
+                        k01[i,j] = 1                
 
-        for j in range (0,3*self.nnos):
-           for i in range (inicio,3*self.nnos):
-                if i == j and k01[i,j] == 0:
-                   k01[i,j] = 1
-                
+        print(k01)       
 
-        print(k01)
 
 class Carregamento_distribuido: 
      
@@ -142,7 +136,7 @@ class Carregamento_distribuido:
         self.w2 = w2
         self.barra = barra
         
-    def calcula_Fepl(self):
+    def calcula_fepl(self):
         L = self.barra.L
         b = L - self.lw - self.a
         wm = (self.w1 + self.w2)/2.0
@@ -155,33 +149,86 @@ class Carregamento_distribuido:
         ra = self.lw*wm-rb
         mb = -(self.lw*(s3*wm+s4*wd))/(120.0*L*L)
         ma = -mb+rb*L-self.a*self.lw*wm-(self.lw*self.lw*(2.0*self.w2+self.w1))/6.0
-        self.barra.fepl[1] = ra
-        self.barra.fepl[2] = ma
-        self.barra.fepl[4] = rb
-        self.barra.fepl[5] = mb
-        #self.barra.fepl[0] = self.barra.fepl[0]+ 1 
+        self.barra.fepl[1] += ra
+        self.barra.fepl[2] += ma
+        self.barra.fepl[4] += rb
+        self.barra.fepl[5] += mb
+        #self.barra.fepl[0] = self.barra.fepl[0]+ 1
 
-class Carregamento_pontual: 
+    def calcula_fep(self):
+        self.barra.fep = np.linalg.inv(self.barra.r)@ self.barra.fepl
+
+class Carregamento_pontual:
+
+    '''
+        Py: componente y da força
+        Px componente x da força
+
+    ''' 
      
-    def __init__(self, a, P, barra):
+    def __init__(self, a, Px, Py, barra):
         self.a = a
-        self.P = P
+        self.Py = Py
+        self.Px = Px
         self.barra = barra
 
-    def calcula_Fepl(self):
+    def calcula_fepl(self):
         L = self.barra.L
         b = L - self.a
         sa = L + 2*self.a
         sb = L + 2*b
-        ra = (self.P*b*b*sa)/(L**3)
-        rb = (self.P*self.a*self.a*sb)/(L**3)
-        ma = (self.P*self.a*b*b)/(L*L)
-        mb = -(self.P*self.a*self.a*b)/(L*L)
-        self.barra.fepl[1] = ra
-        self.barra.fepl[2] = ma
-        self.barra.fepl[4] = rb
-        self.barra.fepl[5] = mb    
-        
+
+        ha = -(self.Px*b*b*sa)/(L**3)
+        hb = -(self.Px*self.a*self.a*sb)/(L**3)
+        ra = (self.Py*b*b*sa)/(L**3)
+        rb = (self.Py*self.a*self.a*sb)/(L**3)
+        ma = (self.Py*self.a*b*b)/(L*L)
+        mb = -(self.Py*self.a*self.a*b)/(L*L)
+
+        self.barra.fepl[0] += ha
+        self.barra.fepl[1] += ra
+        self.barra.fepl[2] += ma
+        self.barra.fepl[3] += hb
+        self.barra.fepl[4] += rb
+        self.barra.fepl[5] += mb
+
+    def calcula_fep(self):
+        self.barra.fep = np.linalg.inv(self.barra.r)@ self.barra.fepl
+
+class Forcas_nodais:
+    '''
+        fnos: lista de tuplas com modulo do carregamento no nó
+        e grau de liberdade correspondente
+
+    '''
+
+    def __init__(self,estrutura,barras,fnos,grestritos):
+        self.nnos = estrutura.nnos
+        self.fnos = fnos
+        self.grestritos = grestritos
+        self.estrutura = estrutura
+        self.barras = barras
+
+    def calcula_fnos(self):
+        for f in self.fnos:
+            self.estrutura.fnos[f[1]] = f[0]
+        for barra in self.barras:
+            barra.calcula_q()
+            for i in range(0,6):
+                self.estrutura.fnos[barra.q[i]] += barra.fep[i]
+
+        print(self.estrutura.fnos)
+
+    def calcula_f01(self):
+        f01 = self.estrutura.fnos
+        for g in self.grestritos:
+            for i in range(0,self.nnos*3):
+                if g == i:
+                    f01[i] = 0
+
+        print(f01)
+
+
 
 #no1 = No(1,60.0,135.0)
 #no2 = No(2,160.0,135.0)
@@ -194,14 +241,31 @@ no3 = No(3,200.0,0.0)
 barra1 = Barra(1.0,no1,no2,10000.0,2.0*5.0,1000.0)
 barra2 = Barra(2.0,no2,no3,10000.0,2.0*5.0,1000.0)
 
+carregamento1 = Carregamento_distribuido(0,100,0.24,0.24,barra1)
+carregamento2 = Carregamento_pontual(62.5,12.0,16.0,barra2)
+
 
 barras = [barra1, barra2]
 grestritos = [0,1,2,6,7,8]
+fnodais = [(10,4),(1000,5)]
 
 portico = Estrutura(barras,3,grestritos)
-
+forcas_nos = Forcas_nodais(portico,barras,fnodais,grestritos)
 portico.calcula_k()
 portico.calcula_k01()
+
+carregamento1.calcula_fepl()
+carregamento1.calcula_fep()
+carregamento2.calcula_fepl()
+carregamento2.calcula_fep()
+
+forcas_nos.calcula_fnos()
+forcas_nos.calcula_f01()
+
+print(barra1.fepl)
+print(barra2.fepl)
+print(barra1.fep)
+print(barra2.fep)
 
 
 #barra1.comprimento_barra()
@@ -217,12 +281,6 @@ portico.calcula_k01()
 #print(barra1.q)
 
 
-carregamento1 = Carregamento_distribuido(0,100,0.24,0.24,barra1)
-carregamento2 = Carregamento_pontual(62.5,16.0,barra2)
-carregamento1.calcula_Fepl()
-carregamento2.calcula_Fepl()
-print(barra1.fepl)
-print(barra2.fepl)
 #carregamento1.mostre()
 #carregamento1.mostre()
 #carregamento1.mostre()
